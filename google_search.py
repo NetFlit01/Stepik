c4676ae65226ae131a60876a00f772d499109687
Nikita <nikita-dock@list.ru>
2023-09-19 21:56:07 +0300

    Добавлены задачи по сортировке пузырьком, выбором и определением корректного введенного номера телефона

--- google_search.py
+++ google_search.py
@@ -9,3 +9,52 @@ for i in ls2:
         if i.lower() not in ls1[j].lower():
             del ls1[j]
 print(*ls1)
+
+
+ls = [4, 9, 7, 1, 0, 10]
+
+#Пузырёк
+for i in range(len(ls) - 1):
+    for j in range(len(ls) - 1 - i):
+        if ls[j] > ls[j + 1]:
+            ls[j], ls[j + 1] = ls[j + 1], ls[j]
+print(ls)
+
+
+#Сортировка выбором
+n = len(ls)
+for i in range(n):
+    #Находим максимальный элемент из неотсортированного списка
+    mx = max(ls[:n - i])
+    ls[n - 1 - i], ls[ls.index(mx)] = mx, ls[n - 1 - i]
+print(ls)
+
+
+#На вход программе подается строка текста. Напишите программу, которая определяет, является ли введенная строка
+# корректным телефонным номером. Строка текста является корректным телефонным номером, если она имеет формат:
+# abc-def-hijk или
+# 7-abc-def-hijk, где a, b, c, d, e, f, h, i, j, k – цифры от 0 до 9
+a = "7-983-432-8563".split("-")
+flag = True
+if len(a) > 2 and len(a) < 5:
+    if a[0] == "7":
+        for i in range(1, len(a) - 1):
+            if a[i].isdigit() and len(a[i]) == 3:
+                continue
+            else:
+                flag = False
+                break
+
+    else:
+        for i in range(len(a) - 1):
+            if a[i].isdigit() and len(a[i]) == 3:
+                continue
+            else:
+                flag = False
+                break
+    if a[-1].isdigit() and len(a[-1]) == 4 and flag:
+        print("YES")
+    else:
+        print("No")
+else:
+    print("NO")
